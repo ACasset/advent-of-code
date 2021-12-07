d1e5df9678291dd356a63c01721517fdb3f47e38
Adrien CASSET <uat6.gitkraken@casset.eu>
2021-12-07 19:37:16 +0100

    Functional day-07

--- 2021/day-07/part-one.js
+++ 2021/day-07/part-one.js
@@ -1,3 +1,29 @@
 function execute( input ) {
-    return "NYI";
+    let crabs = input.split(",");
+
+    let maxIndex = 0;
+
+    for (var i = 0; i < crabs.length; i++) {
+        if (maxIndex < crabs[i]) {
+            maxIndex = crabs[i];
+        }
+    }
+
+    let bestCost = 9999999;
+    let bestCostIndex = -1;
+
+    for (var i = 0; i <= maxIndex; i++) {
+        let currentCost = 0;
+
+        for (var j = 0; j < crabs.length; j++) {
+            currentCost += Math.abs(crabs[j] - i);
+        }
+
+        if (currentCost < bestCost) {
+            bestCost = currentCost;
+            bestCostIndex = i;
+        }
+    }
+
+    return bestCost;
 }
--- 2021/day-07/part-two.js
+++ 2021/day-07/part-two.js
@@ -1,3 +1,33 @@
 function execute( input ) {
-    return "NYI";
+    let crabs = input.split(",");
+
+    let maxIndex = 0;
+
+    for (var i = 0; i < crabs.length; i++) {
+        if (maxIndex < crabs[i]) {
+            maxIndex = crabs[i];
+        }
+    }
+
+    let bestCost = 99999999999999;
+    let bestCostIndex = -1;
+
+    for (var i = 0; i <= maxIndex; i++) {
+        let currentCost = 0;
+
+        for (var j = 0; j < crabs.length; j++) {
+            let distance = Math.abs(crabs[j] - i);
+
+            for (var k = distance; k > 0; k--) {
+                currentCost += k;
+            }
+        }
+
+        if (currentCost < bestCost) {
+            bestCost = currentCost;
+            bestCostIndex = i;
+        }
+    }
+
+    return bestCost;
 }
