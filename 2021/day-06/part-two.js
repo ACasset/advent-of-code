2f9331d7d08ca83b6b341eee9352df5d54654edf
Adrien CASSET <uat6.gitkraken@casset.eu>
2021-12-07 19:21:11 +0100

    Functional day-06 + inputs day-07

--- 2021/day-06/part-two.js
+++ 2021/day-06/part-two.js
@@ -1,3 +1,38 @@
 function execute( input ) {
-    return "result";
+    let inputSchool = input.split(",");
+
+    let initialSchool = [];
+
+    for (var i = 0; i <= 8; i++) {
+        initialSchool[i] = inputSchool.filter(x => x == i).length;
+    }
+
+    console.log(initialSchool);
+
+    for (var i = 1; i <= 256; i++) {
+        let currentSchool = [];
+
+        for (var j = 0; j <= 8; j++) {
+            if (j == 6)
+                currentSchool[j] = initialSchool[7] + initialSchool[0];
+            else if (j == 8)
+                currentSchool[j] = initialSchool[0];
+            else
+                currentSchool[j] = initialSchool[j+1];
+        }
+
+        initialSchool = currentSchool;
+    }
+
+    return currentSchoolSum(initialSchool);
+}
+
+function currentSchoolSum(school) {
+    let sum = 0;
+
+    school.forEach(function(ttl) {
+        sum += ttl;
+    });
+
+    return sum;
 }
