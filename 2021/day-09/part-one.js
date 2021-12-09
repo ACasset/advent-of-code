e2da0f488bf21f6c06d30df204e034638eb3afcc
ACasset <39308904+ACasset@users.noreply.github.com>
2021-12-09 13:59:30 +0100

    Complete day-09

--- 2021/day-09/part-one.js
+++ 2021/day-09/part-one.js
@@ -1,3 +1,37 @@
 function execute( input ) {
-    return "NYI";
+    let lines = input.split("\n");
+	
+	lines.forEach(function(line, index) {
+		lines[index] = line.split("");
+	});
+	
+	let lowestHeights = [];
+	
+	for (var y = 0; y < lines.length; y++) {
+		for (var x = 0; x < lines[y].length; x++) {
+			let currentHeight = lines[y][x];
+			
+			if (x >= 1 && parseInt(lines[y][x-1]) <= parseInt(currentHeight))
+				continue;
+			
+			if (x < (lines[y].length - 1) && parseInt(lines[y][x+1]) <= parseInt(currentHeight))
+				continue;
+			
+			if (y >= 1 && parseInt(lines[y-1][x]) <= parseInt(currentHeight))
+				continue;
+			
+			if (y < (lines.length - 1) && parseInt(lines[y+1][x]) <= parseInt(currentHeight))
+				continue;
+			
+			lowestHeights.push(currentHeight);
+		}
+	}
+	
+	let sum = lowestHeights.length;
+	
+	lowestHeights.forEach(function(lowestHeight) {
+		sum += parseInt(lowestHeight);
+	});
+	
+	return sum;
 }
--- 2021/day-09/part-two.js
+++ 2021/day-09/part-two.js
@@ -1,3 +1,66 @@
 function execute( input ) {
-    return "NYI";
+    let lines = input.split("\n");
+	
+	lines.forEach(function(line, y) {
+		lines[y] = line.split("");
+		lines[y].forEach(function(value, x) {
+			if (value == 9)
+				lines[y][x] = true;
+			else
+				lines[y][x] = false;
+		});
+	});
+	
+	let unexploredCount = countUnexplored(lines);
+	let basinsSizes = [];
+	
+	for (var y = 0; y < lines.length; y++) {
+		for (var x = 0; x < lines[y].length; x++) {
+			if (!lines[y][x]) {
+				exploreBasin(lines, y, x);
+				let currentUnexploredCount = countUnexplored(lines);
+				basinsSizes.push( parseInt(unexploredCount - currentUnexploredCount));
+				unexploredCount = currentUnexploredCount;
+			}
+		}
+	}
+	
+	basinsSizes = basinsSizes.sort(function compare(a, b) {
+		return a - b;
+	});
+	
+	let basinsCount = basinsSizes.length;
+	
+	return basinsSizes[basinsCount-1] * basinsSizes[basinsCount-2] * basinsSizes[basinsCount-3];
+}
+
+function countUnexplored(lines) {
+	let count = 0;
+	
+	for (var y = 0; y < lines.length; y++) {
+		for (var x = 0; x < lines[y].length; x++) {
+			if (!lines[y][x])
+				count++;
+		}
+	}
+	
+	return count;
+}
+
+function exploreBasin(lines, y, x) {
+	if (y < 0 || y >= lines.length)
+		return;
+	
+	if (x < 0 || x >= lines[y].length)
+		return;
+	
+	if (lines[y][x])
+		return;
+	
+	lines[y][x] = true;
+	
+	exploreBasin(lines, y-1, x);
+	exploreBasin(lines, y+1, x);
+	exploreBasin(lines, y, x-1);
+	exploreBasin(lines, y, x+1);
 }
