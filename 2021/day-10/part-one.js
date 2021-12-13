4dc4140bb6c1d9be4a6fbb96f921780eb41e77e7
ACasset <39308904+ACasset@users.noreply.github.com>
2021-12-13 12:25:43 +0100

    Working day-10

--- 2021/day-10/part-one.js
+++ 2021/day-10/part-one.js
@@ -1,3 +1,45 @@
 function execute( input ) {
-    return "NYI";
+    let lines = input.split("\n");
+	
+	let causes = { ")": 0, "}": 0, "]": 0, ">": 0 };
+	
+	lines.forEach(function(line) {
+		let expected = [];
+		
+		line.split("").forEach(function(character) {
+			switch (character) {
+				// Opening
+				
+				case "(":
+					expected.push(")");
+				break;
+				
+				case "{":
+					expected.push("}");
+				break;
+				
+				case "[":
+					expected.push("]");
+				break;
+				
+				case "<":
+					expected.push(">");
+				break;
+				
+				// Closing
+				
+				case ")":
+				case "}":
+				case "]":
+				case ">":
+					if (expected.pop() != character) {
+						causes[character] += 1;
+						return;
+					}
+				break;
+			}
+		});
+	});
+	
+	return causes[")"] * 3 + causes["]"] * 57 + causes["}"] * 1197 + causes[">"] * 25137;
 }
--- 2021/day-10/part-two.js
+++ 2021/day-10/part-two.js
@@ -1,3 +1,79 @@
 function execute( input ) {
-    return "NYI";
+    let lines = input.split("\n");
+	
+	let scores = [];
+	
+	lines.forEach(function(line) {
+		let expected = [];
+		let characters = line.split("");
+		
+		for (var i = 0; i < characters.length; i++) {
+			switch (characters[i]) {
+				// Opening
+				
+				case "(":
+					expected.push(")");
+				break;
+				
+				case "[":
+					expected.push("]");
+				break;
+				
+				case "{":
+					expected.push("}");
+				break;
+				
+				case "<":
+					expected.push(">");
+				break;
+				
+				// Closing
+				
+				case ")":
+				case "}":
+				case "]":
+				case ">":
+					if (expected.pop() != characters[i]) {
+						expected = [];
+						i = characters.length;
+					}
+				break;
+			}
+		}
+		
+		if (expected.length > 0) {
+			expected.reverse();
+			let score = 0;
+			
+			for (var i = 0; i < expected.length; i++) {
+				score *= 5;
+				
+				switch (expected[i]) {
+					case ")":
+						score += 1;
+					break;
+					
+					case "]":
+						score += 2;
+					break;
+					
+					case "}":
+						score += 3;
+					break;
+					
+					case ">":
+						score += 4;
+					break;
+				}
+			}
+			
+			scores.push(score);
+		}
+	});
+	
+	scores = scores.sort(function compare(a, b) {
+		return a - b;
+	});
+	
+	return scores[(scores.length-1)/2];
 }
