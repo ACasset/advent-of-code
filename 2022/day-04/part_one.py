87d49479b0ba779c9830671e0fc87c0e1b6cfd0b
ACasset <39308904+ACasset@users.noreply.github.com>
2022-12-04 17:29:28 +0000

    2022-12-04

--- 2022/day-04/part_one.py
+++ 2022/day-04/part_one.py
@@ -6,9 +6,10 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 import functions
+import numpy
 
 DAY = "04"
-INPUT_TYPE = "test"
+INPUT_TYPE = "real"
 
 def main():
     '''Main function'''
@@ -18,11 +19,21 @@ def main():
 
     for line in input_file.split("\n"):
         assignments = line.split(",")
-        first_assignment = range(assignments[0].split('-')[0], assignments[0].split('-')[1])
-        second_assignment = range(assignments[1].split('-')[0], assignments[1].split('-')[1])
-        print(first_assignment)
-        print(second_assignment)
+        
+        first_assignment = [*range(int(assignments[0].split('-')[0]), int(assignments[0].split('-')[1])+1)]
+        second_assignment = [*range(int(assignments[1].split('-')[0]), int(assignments[1].split('-')[1])+1)]
+
+        overlapping_assignments += check_if_in(first_assignment, second_assignment)
 
     print(overlapping_assignments)
+
+def check_if_in(first_list, second_list):
+    if (first_list[0] in second_list and first_list[len(first_list)-1] in second_list):
+        return 1
+    
+    if (second_list[0] in first_list and second_list[len(second_list)-1] in first_list):
+        return 1
+
+    return 0
 
 main()
--- 2022/day-04/part_two.py
+++ 2022/day-04/part_two.py
@@ -8,10 +8,31 @@ sys.path.append(parent)
 import functions
 
 DAY = "04"
-INPUT_TYPE = "test"
+INPUT_TYPE = "real"
 
 def main():
     '''Main function'''
     input_file = functions.read_file(DAY, INPUT_TYPE)
+
+    overlapping_assignments = 0
+
+    for line in input_file.split("\n"):
+        assignments = line.split(",")
+        
+        first_assignment = [*range(int(assignments[0].split('-')[0]), int(assignments[0].split('-')[1])+1)]
+        second_assignment = [*range(int(assignments[1].split('-')[0]), int(assignments[1].split('-')[1])+1)]
+
+        overlapping_assignments += check_if_in(first_assignment, second_assignment)
+
+    print(overlapping_assignments)
+
+def check_if_in(first_list, second_list):
+    if (first_list[0] in second_list or first_list[len(first_list)-1] in second_list):
+        return 1
+    
+    if (second_list[0] in first_list or second_list[len(second_list)-1] in first_list):
+        return 1
+
+    return 0
 
 main()
