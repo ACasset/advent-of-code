a924a9d817af3294889339ed21d868077441f1f2
ACasset <39308904+ACasset@users.noreply.github.com>
2024-06-26 15:20:19 +0200

    2023/03/1 candidate

--- 2023/day-03/part-1.rb
+++ 2023/day-03/part-1.rb
@@ -6,20 +6,20 @@ def is_symbol?(char)
     char.match?(/\#$%&*+\-\/=@]/)
 end
 
-def find_numbers(line)
+def find_numbers(map, line_index)
+    line = map[line_index]
     numbers = []
     number = ""
+    number_start_index = 0
 
     (0..line.length-1).each { |i|
-        puts "Character: #{line[i]}"
-
         if is_number?(line[i])
+            number_start_index = i
             number += line[i]
         else
             unless number.empty?
-                puts "Adding number: #{number}"
-                numbers.push(number)
-                number = ""
+                if has_symbol_around?(map, line_index, number_start_index, i-1)
+                    numbers.push(number.to_i)
             end
         end
     }
@@ -27,19 +27,18 @@ def find_numbers(line)
     return numbers
 end
 
-def has_symbol_around?(map, x, y)
-    puts "Starting point is: #{x},#{y}"
-
-    low_x_range = [0, x-1].max
-    high_x_range = [x+1, map[0].length-1].min
-    low_y_range = [0, y-1].max
-    high_y_range = [y+1, map.length-1].min
+def has_symbol_around?(map, line_index, column_start_index, column_end_index)
+    low_x_range = [0, column_start_index-1].max
+    high_x_range = [column_end_index+1, map[0].length-1].min
+    low_y_range = [0, line_index-1].max
+    high_y_range = [line_index+1, map.length-1].min
 
     puts "Range will be from #{low_x_range},#{low_y_range} to #{high_x_range},#{high_y_range}"
 
     (low_y_range..high_y_range).each { |i|
         (low_x_range..high_x_range).each { |j|
-            puts "Looking at #{j},#{i}: #{map[i][j]}"
+            if is_symbol?(map[i][j])
+                return true
         }
     }
 
@@ -50,10 +49,7 @@ map = get_input_lines()
 sum = 0
 
 (0..map.length-1).each { |i|
-    puts "Looking for line #{i}: #{map[i]}"
-    numbers = find_numbers(map[i])
-
-    puts "Numbers for line #{i}: #{numbers}"
+    sum += find_numbers(map, i)
 
     #(0..map[i].length-1).each { |j|
     #    
