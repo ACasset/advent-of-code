1acfc2b5921d5508196ce1db1e94c34f21fc9b75
ACasset <39308904+ACasset@users.noreply.github.com>
2024-06-26 13:23:59 +0200

    Cleanup

--- 2023/day-03/part-1.rb
+++ 2023/day-03/part-1.rb
@@ -1,41 +1,20 @@
 def has_symbol_around?(map, x, y)
     puts "Starting point is: #{x},#{y}"
 
-    low_x_range = x-1
-    high_x_range = x+1
-    low_y_range = y-1
-    low_y_range = y+1
+    low_x_range = [0, x-1].max
+    high_x_range = [x+1, map[0].length].min
+    low_y_range = [0, y-1].max
+    high_y_range = [y+1, map.length].min
 
-    i in ([0, low_y_range].max)..([map.length, low_y_range].min) do
-        j in [0, low_x_range].max..[map[i].length, high_x_range].min do
+    puts "Range will be from #{low_x_range},#{low_y_range} to #{high_x_range},#{high_y_range}"
+
+    i in low_y_range..high_y_range do
+        j in low_x_range..high_x_range do
             puts "Looking at #{j},#{i}: #{map[j][i]}"
         end
     end
 
     return false
-end
-
-def parse_line(line)
-    target = { "red" => 12, "green" => 13, "blue" => 14 }
-
-    parsing = line.scan(/^Game (\d+): ([\d\w ,;]+)$/).flatten
-    game_id = parsing.first
-    game_data = parsing.last
-    draws = game_data.split("; ")
-
-    draws.each { |draw|
-        groups = draw.split(", ")
-
-        groups.each { |group|
-            color = group.split(" ").last
-
-            if (group.split(" ").first.to_i > target[color])
-                return 0
-            end
-        }
-    }
-
-    return game_id.to_i
 end
 
 has_symbol_around(get_input_lines(), 2, 3)
